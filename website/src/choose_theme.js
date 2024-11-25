a8a3af0a9ed91060b54e2a091d4654c56bed983f
Vincent Da Conceicao Ramos <vda-conc@made-f0Ar4s7.clusters.42paris.fr>
2024-11-25 01:59:27 +0100

    implementing light and dark theme

--- website/src/choose_theme.js
+++ website/src/choose_theme.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { NavBar } from './navbar';
 import { Footer } from './footer';
 import ReactDOM from 'react-dom/client';
+import './assets/styles/choose_theme.css'
 
-const cookies = document.cookie;
 
-console.log(cookies);
-
-const isThemeSet = (cookies) =>
+const isThemeSet = () =>
 {
-  if (!cookies.length())
-		return 0;
+	const cookies = document.cookie;
+	return cookies.split(';').some(el => el.trim() === "theme=light");
 }
 
 export function ChooseTheme()
@@ -18,8 +16,8 @@ export function ChooseTheme()
 	return (
 		<>
 		<NavBar />
-		<main>
-			{isThemeSet ? <LightTheme /> : <DarkTheme />}
+		<main id='themeMain'>
+			{isThemeSet() ? <DarkTheme /> : <LightTheme /> }
 		</main>
 		<Footer />
 		</>
@@ -30,11 +28,11 @@ function LightTheme()
 {
 	return (
 		<>
-		<form action='/themelight' method="post">
-			<button type="submit">Switch to Light theme</button>
+		<form className='themeForm' action='/themelight' method="post">
+			<button className='themeSubmitBtn' type="submit">Switch to Light theme</button>
 			<input type="hidden" name="theme" value="light" />
-
 		</form>
+		<p className='themeText'>Close your eyes! Light mode incoming! 💡 </p>
 		</>
 	)
 }
@@ -43,10 +41,11 @@ function DarkTheme()
 {
 	return (
 		<>
-		<form action='/darkTheme' method="post">
-			<button type="submitBtn">Switch to Dark theme</button>
+		<form className='themeForm' action='/themedark' method="post">
+			<button className='themeSubmitBtn' type="submit">Switch to Dark theme</button>
 			<input type="hidden" name="theme" value="dark" />
 		</form>
+		<p className='themeText'>Enter the dark side... 👨‍💻 </p>
 		</>
 	)
 }
