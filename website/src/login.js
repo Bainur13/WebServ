c772bf0444d1d9a5f38ac078da4c2e681b8ffd02
Vincent Da Conceicao Ramos <vda-conc@paul-f4Br5s6.clusters.42paris.fr>
2024-12-06 16:50:44 +0100

    style improved

--- website/src/login.js
+++ website/src/login.js
@@ -4,7 +4,9 @@ import { Footer } from './footer.js';
 import './assets/styles/404.css';
 import { Link } from 'react-router-dom';
 import {isThemeSet} from './choose_theme';
-import './assets/styles/login.css'
+import './assets/styles/login.css';
+import './assets/styles/custompage.css';
+
 
 const isLightTheme = isThemeSet();
 
@@ -16,7 +18,7 @@ export function Login()
 		<main id={isLightTheme ? 'loginMainLight' : 'loginMain'}>
 			<h1 id='loginTitle'>Login</h1>
 			<LoginForm />
-			<Link to="/createaccount">
+			<Link className={isLightTheme ? 'switchLinksLight' : 'switchLinks'} to="/createaccount">
 				<div>Don't have an account yet? Sign Up</div>
 			</Link>
 		</main>
@@ -29,18 +31,18 @@ function LoginForm()
 {
 	return (
 		<>
-			<form id={ isLightTheme ? 'loginFormLight' : "loginForm"} action="/connect" method="POST">
+			<form className='loginForm' action="/connect" method="POST">
 				<div id='internFormDiv'>
 					<div id='formLabels'>
 						<label> Username </label>
 						<label> Password </label>
 					</div>
-					<div id='formInputs'>
+					<div id={ isLightTheme ? 'formInputsLight' : 'formInputs'}>
 						<input type="text" name="username" required></input>
 						<input type="password" name="password" required></input>
 					</div>
 				</div>
-				<button id={isLightTheme ? "creationSubmitBtnLight" : "creationSubmitBtn"} type="submit">Login</button>
+				<button id={isLightTheme ? "formSubmitBtnLight" : "formSubmitBtn"} type="submit">Login</button>
 			</form>
 		</>
 	)
--- website/src/signup.js
+++ website/src/signup.js
@@ -4,7 +4,8 @@ import { Footer } from './footer.js';
 import './assets/styles/404.css';
 import {isThemeSet} from './choose_theme';
 import { Link } from 'react-router-dom';
-import './assets/styles/signup.css'
+import './assets/styles/signup.css';
+import './assets/styles/custompage.css';
 
 const isLightTheme = isThemeSet();
 
@@ -16,7 +17,7 @@ export function CreateAccount()
 		<main id={isLightTheme ? 'accCreationMainLight' : 'accCreationMain'}>
 			<h1 id='accCreationTitle'>Create an account</h1>
 			<AccountCreationForm />
-			<Link to="/login">
+			<Link className={isLightTheme ? 'switchLinksLight' : 'switchLinks'} to="/login">
 				<div>Already have an account ? Log here</div>
 			</Link>
 		</main>
@@ -29,18 +30,18 @@ function AccountCreationForm()
 {
 	return (
 		<>
-			<form id={ isLightTheme ? 'accCreationFormLight' : "accCreationForm"} action="/signup" method="POST">
+			<form className="accCreationForm" action="/signup" method="POST">
 				<div id='internFormDiv'>
 					<div id='formLabels'>
 						<label> Username </label>
 						<label> Password </label>
 					</div>
-					<div id='formInputs'>
+					<div id={ isLightTheme ? 'formInputsLight' : 'formInputs'}>
 						<input type="text" name="username" required></input>
 						<input type="password" name="password" required></input>
 					</div>
 				</div>
-				<button id={isLightTheme ? "creationSubmitBtnLight" : "creationSubmitBtn"} type="submit">Create</button>
+				<button id={isLightTheme ? "formSubmitBtnLight" : "formSubmitBtn"} type="submit">Create</button>
 			</form>
 		</>
 	)
